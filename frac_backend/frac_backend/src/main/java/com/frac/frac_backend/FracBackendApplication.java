f3e0b8f0d7a6a02b7427682ac69f6641b85bb458
Kavindu0518 <kavinducgamage@gmail.com>
2025-12-10 14:22:50 +0530

    git fill uploaded

---
+++ frac_backend/frac_backend/src/main/java/com/frac/frac_backend/FracBackendApplication.java
@@ -0,0 +1,13 @@
+package com.frac.frac_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FracBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FracBackendApplication.class, args);
+	}
+
+}
---
+++ frac_backend/frac_backend/src/test/java/com/frac/frac_backend/FracBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frac.frac_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FracBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
